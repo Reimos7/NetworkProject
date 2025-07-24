cb0403fbde005e895154c89b9912e2b6c1cb4c12
Reimos7 <tjrvud9@gmail.com>
2025-07-24 18:35:28 +0900

    chore: gitignore 추가

---
+++ NetworkProject/APIKey.swift
@@ -0,0 +1,8 @@
+//
+//  APIKey.swift
+//  NetworkProject
+//
+//  Created by Reimos on 7/24/25.
+//
+
+import Foundation
--- NetworkProject/Lotto/Lotto.swift
+++ NetworkProject/Lotto/Lotto.swift
@@ -7,7 +7,14 @@
 
 import UIKit
 
+// Decodable -> struct가 외부에서 오는 데이터를 담는다
 struct Lotto {
+    let drwNoDate: String
+    let bnusNo: Int
+}
+
+struct LottoBall {
     let number: Int
     let color: UIColor
 }
+
